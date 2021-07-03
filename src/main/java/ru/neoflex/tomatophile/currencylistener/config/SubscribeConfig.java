61efc4284eba64685751266cb8a126225310c9dc
Stroev Artyom <stroev8artyom@gmail.com>
2021-07-03 11:36:31 +0300

    config lists of subscribe in memory

---
+++ src/main/java/ru/neoflex/tomatophile/currencylistener/config/SubscribeConfig.java
@@ -0,0 +1,22 @@
+package ru.neoflex.tomatophile.currencylistener.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import ru.neoflex.tomatophile.currencylistener.pojo.Subscribe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableScheduling
+public class SubscribeConfig {
+    @Bean
+    public List<Subscribe> subscribesOnUpdate(){
+        return new ArrayList<>();
+    }
+    @Bean
+    public List<Subscribe> subscribesOnFall(){
+        return new ArrayList<>();
+    }
+}
