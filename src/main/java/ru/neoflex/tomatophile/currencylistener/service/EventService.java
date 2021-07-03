64e9d5e46cf289ad8e249ab9a9f1ea3e0c050f5c
Stroev Artyom <stroev8artyom@gmail.com>
2021-07-03 11:39:50 +0300

    service to send Events to bot service

---
+++ src/main/java/ru/neoflex/tomatophile/currencylistener/service/EventService.java
@@ -0,0 +1,68 @@
+package ru.neoflex.tomatophile.currencylistener.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import ru.neoflex.tomatophile.currencylistener.pojo.Event;
+import ru.neoflex.tomatophile.currencylistener.pojo.Subscribe;
+
+import java.util.List;
+
+@Service
+public class EventService {
+    @Value("${cryptoKoshBot.url}")
+    private String cryptoKoshBotUrl;
+
+    private final List<Subscribe> subscribesOnUpdate;
+    private final List<Subscribe> subscribesOnFall;
+
+    private final RestTemplate restTemplate;
+
+    private final CoinMarketCapService coinMarketCapService;
+
+
+    public EventService(List<Subscribe> subscribesOnUpdate, List<Subscribe> subscribesOnFall, RestTemplateBuilder restTemplateBuilder, CoinMarketCapService coinMarketCapService) {
+        this.subscribesOnUpdate = subscribesOnUpdate;
+        this.subscribesOnFall = subscribesOnFall;
+        this.restTemplate = restTemplateBuilder.build();
+        this.coinMarketCapService = coinMarketCapService;
+    }
+
+    @Scheduled(fixedRate = 3000)
+    public void updateEvent() {
+        var url = cryptoKoshBotUrl.concat("event/update");
+
+        for (var subscribe : subscribesOnUpdate) {
+            var chatId = subscribe.getChatId();
+            var figi = subscribe.getFigi();
+
+            var event = Event.builder().chatId(chatId).figi(figi).price(coinMarketCapService.getOneByFigi(figi).getPrice()).build();
+
+            restTemplate.postForObject(url, event, Event.class);
+        }
+    }
+
+    @Scheduled(fixedRate = 30000)
+    public void fallEvent() {
+        var url = cryptoKoshBotUrl.concat("event/fall");
+
+        for (var i = 0; i < subscribesOnFall.size(); i++) {
+            var chatId = subscribesOnFall.get(i).getChatId();
+            var figi = subscribesOnFall.get(i).getFigi();
+
+            var lastCurrency = subscribesOnFall.get(i);
+            var currCurrency = coinMarketCapService.getOneByFigi(lastCurrency.getFigi());
+
+            if(lastCurrency.getLastPrice() - lastCurrency.getLastPrice()*lastCurrency.getFallPercent()/100 >= currCurrency.getPrice()){
+                subscribesOnFall.remove(lastCurrency);
+                i--;
+
+                var event = Event.builder().chatId(chatId).figi(figi).price(currCurrency.getPrice()).build();
+
+                restTemplate.postForObject(url, event, Event.class);
+            }
+        }
+    }
+}
